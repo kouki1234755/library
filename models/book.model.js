a51c37b3b154bed35f0cbc252fd5c58b8c5e55b6
imen123 <kouki.imen2025@gmail.com>
2025-06-18 15:36:49 +0200

    changes

---
+++ models/book.model.js
@@ -0,0 +1,8 @@
+
+const mongoose = require('mongoose');
+const bookSchema = new mongoose.Schema({
+title: String,
+author: String,
+userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+module.exports = mongoose.model('Book', bookSchema);
